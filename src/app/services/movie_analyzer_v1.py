79d7306c755547dca868e83ce91d6a2529352e75
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2026-01-13 16:07:35 +0100

    update

--- src/app/services/movie_analyzer_v1.py
+++ src/app/services/movie_analyzer_v1.py
@@ -2,8 +2,8 @@
 async def analyze_movie(movie_id: str) -> dict:
     return {
         "id": movie_id,
-        "ai_summary": "Ceci est un résumé statique du film.",
-        "ai_opinion_summary": "Ceci est un résumé statique des avis.",
-        "ai_best_genre": "Action",
-        "ai_tags": ["tag1", "tag2", "tag3"]
+        "aiSummary": "Ceci est un résumé statique du film.",
+        "aiOpinionSummary": "Ceci est un résumé statique des avis.",
+        "aiBestGenre": "Action",
+        "aiTags": ["tag1", "tag2", "tag3"]
     }
